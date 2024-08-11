dcde199a1fb937946a3e825146627df0ec89dca0
FundaGungorAlkan <fundaalkan112@gmail.com>
2024-08-11 22:21:54 +0100

    .

--- src/components/info.js
+++ src/components/info.js
@@ -1,18 +1,24 @@
 import { WiSunset } from 'react-icons/wi';
 
 
-function Info() {
+function Info({info}) {
+
+
+   if (!info || !info.main || !info.weather) {
+      return <div></div>; // Veri yükleniyorken bir yükleniyor mesajı gösterilebilir.
+  }
+
 
 
 return (
 <div className="info">
-    <p id="sehir">Şehir, Ülke</p>
+    <p id="sehir">{info.name}</p>
     <div className="genelDeger">
-  <p id="sicaklik">--</p><WiSunset className="fa-c"/>
+  <p id="sicaklik">{info.main.temp}</p><WiSunset className="fa-c"/>
  </div>
- <p id="havaDurumu">Hava Durumu</p>
+ <p id="havaDurumu">{info.weather[0].description}</p>
  <div className="his">
-    <p id="hissedilen">Hissedilen</p><WiSunset className="fa-c"/>
+    <p id="hissedilen">{info.main.feels_like}</p><WiSunset className="fa-c"/>
  </div>
 </div>
 
